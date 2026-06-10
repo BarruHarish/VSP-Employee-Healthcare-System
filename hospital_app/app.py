f5fa5c0c694c581b47136b9593b000c1fdae65ae
B.HARISH <bharish1214@gmail.com>
2026-06-10 11:57:52 +0530

    Update app.run to use environment port

--- hospital_app/app.py
+++ hospital_app/app.py
@@ -458,5 +458,6 @@ def admin_reports():
     
     return render_template('admin_reports.html', chart_data=chart_data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
